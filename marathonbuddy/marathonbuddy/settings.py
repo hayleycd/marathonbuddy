480e7326ec58d81ba915506f7e4d817e88dfcc81
hayleycd <cook.hayley@gmail.com>
2018-09-30 19:59:11 -0700

    Changing some production stuff

--- marathonbuddy/marathonbuddy/settings.py
+++ marathonbuddy/marathonbuddy/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dev_key'
+SECRET_KEY = os.environ(['KEY'])
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -64,8 +64,11 @@ WSGI_APPLICATION = 'marathonbuddy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'default',
+        'USER': os.environ['MYSQLUSER'],
+        'PASSWORD': os.environ['MYSQLPASS'],
+        'HOST': os.environ['MYSQLHOST'],
     }
 }
 
