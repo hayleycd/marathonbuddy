bfbfb1c53128cdde2c2cdff1ed43ed0cb9755167
hayleycd <cook.hayley@gmail.com>
2018-10-21 15:38:44 -0700

    fix try-except statments

--- marathonbuddy/marathonbuddy/settings.py
+++ marathonbuddy/marathonbuddy/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try os.environ['KEY']:
+try:
     SECRET_KEY = os.environ['KEY']
 except:
     SECRET_KEY = "testingkey"
@@ -65,17 +65,17 @@ WSGI_APPLICATION = 'marathonbuddy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-try os.environ['MYSQLUSER']:
+try:
     os_user = os.environ['MYSQLUSER']
 except:
     os_user = "testinguser"
 
-try os.environ['MYSQLPASS']:
+try:
     os_sql_pass = os.environ['MSQLPASS']
 except:
     os_sql_pass = "testingpass"
 
-try os.environ['MYSQLHOST']:
+try:
     os_host = os.environ['MYSQLHOST']
 except:
     os_host = "testinghost"
