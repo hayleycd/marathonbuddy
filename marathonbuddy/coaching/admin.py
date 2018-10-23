aa11fe6037fffeaa7b3867f57047731fa1c1cfcb
Hayley Cook <hayley.denbraver@brightscope.com>
2018-10-22 20:54:49 -0700

    whatevs

--- marathonbuddy/coaching/admin.py
+++ marathonbuddy/coaching/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Cheer, RunUpdate, RunEvent, Coaching, Picture, Compliment
+from .models import Cheer, RunUpdate, RunEvent, Picture, Compliment
 
 admin.site.register(Cheer)
 admin.site.register(RunUpdate)
 admin.site.register(RunEvent)
-admin.site.register(Coaching)
 admin.site.register(Picture)
 admin.site.register(Compliment)
