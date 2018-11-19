ef1f6ed81f02c1c170325e54c485280df9d78d33
hayleycd <cook.hayley@gmail.com>
2018-11-18 16:09:15 -0800

    making admin nicer

--- marathonbuddy/coaching/admin.py
+++ marathonbuddy/coaching/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
 from .models import Cheer, RunUpdate, RunEvent, Picture, Compliment, Visitor
 
-admin.site.register(Cheer)
-admin.site.register(RunUpdate)
-admin.site.register(RunEvent)
-admin.site.register(Picture)
-admin.site.register(Compliment)
+class CheerAdmin(admin.ModelAdmin):
+	list_display = ('text_body', 'cheerleader',)
+
+
+class RunUpdateAdmin(admin.ModelAdmin):
+	list_display = ('text_body', 'time_stamp',)
+
+
+class RunEventAdmin(admin.ModelAdmin):
+	list_display = ('event_name', 'event_date',)
+
+
+class PictureAdmin(admin.ModelAdmin):
+	list_display = ('description',)
+
+
+class ComplimentAdmin(admin.ModelAdmin):
+	list_display = ('Compliment',)
+
+
+class VisitorAdmin(admin.ModelAdmin):
+	list_display = ('nickname', 'how',)
+admin.site.register(Cheer, CheerAdmin)
+admin.site.register(RunUpdate, RunUpdateAdmin)
+admin.site.register(RunEvent, RunEventAdmin)
+admin.site.register(Picture, PictureAdmin)
+admin.site.register(Compliment, ComplimentAdmin)
 admin.site.register(Visitor)
